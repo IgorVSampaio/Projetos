2593b2510766b216de8f97de5f9606c9e5f1101e
IgorSampaio <Igor.vsampaio@gmail.com>
2023-09-03 00:18:16 -0300

    Projeto

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Esse é o Screen Match ");
+        System.out.println("Filme: Top Gun: Maverick ");
+// Tipo - variavel = valo
+//            /Padrão camelcase\
+        int anoDeLancamento = 2022;          //conctenação
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        boolean incluidoNoplano = true;
+        double notaDoFilme = 8.1;
+
+
+        double media = (9.8 + 6.3 + 8.0) /3;
+        System.out.println(media);
+        String sinopse;
+        System.out.println("Filme top Gun\n" +
+                "                Filme de aventura com galã dos anos 80\n" +
+                "                Muito bom\n" +
+                "                Ano de lançamento\n              " +
+                "  " + anoDeLancamento);
+
+
 
     }
 }
